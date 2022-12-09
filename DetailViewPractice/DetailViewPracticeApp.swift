32842df66e6d3a11dc8ba24129fedcd51104acb9
blakeCS <mburton@blakeschool.org>
2022-12-09 08:36:32 -0600

    Initial Commit

---
+++ DetailViewPractice/DetailViewPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DetailViewPracticeApp.swift
+//  DetailViewPractice
+//
+//  Created by Madeline Burton on 12/9/22.
+//
+
+import SwiftUI
+
+@main
+struct DetailViewPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
